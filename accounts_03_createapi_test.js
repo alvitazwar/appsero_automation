b0d99dc85fd924824e0395bcb549b8bce1b9250e
alvitazwar <alvitazwar@wedevs.com>
2020-11-10 14:58:46 +0600

    api key automation

---
+++ accounts_03_createapi_test.js
@@ -0,0 +1,16 @@
+Feature('Appsero');
+
+Scenario('@accounts api create ',async (I) => {
+  I.amOnPage('https://staging.appsero.com/login');
+  I.fillField('Email Address', 'alvitazwar@wedevs.com');
+  I.fillField('Password','crisis052');
+  I.click('Log in');
+  I.see('Plugins');
+  I.click('//*[@id="app"]/section/aside/div/div[2]/ul/li[6]/a');
+  I.click('//div[@class="ant-row-flex ant-row-flex-middle ant-row-flex-space-between page-heading"]/button[@type="button"]');
+  I.see('Not connected');
+
+
+
+
+}).tag('@accounts');
---
+++ accounts_04_deleteapi_test.js
@@ -0,0 +1,18 @@
+Feature('Appsero');
+
+Scenario('@accounts api delete ',async (I) => {
+  I.amOnPage('https://staging.appsero.com/login');
+  I.fillField('Email Address', 'alvitazwar@wedevs.com');
+  I.fillField('Password','crisis052');
+  I.click('Log in');
+  I.see('Plugins');
+  I.click('//*[@id="app"]/section/aside/div/div[2]/ul/li[6]/a');
+  I.see('Not connected');
+  I.click('//*[@id="app"]//div[2]/table/tbody/tr[2]/td[4]/button');
+  I.click('Yes');
+  I.see('API Key Deleted Successfully.');
+
+
+
+
+}).tag('@accounts');
