0b641864e9f3534ed817cd01811215fb793d5473
[alvitazwar] <[alvitazwar@gmail.com]>
2020-12-10 20:28:50 +0600

    test new things

--- steps_file.js
+++ steps_file.js
@@ -16,6 +16,34 @@ module.exports = function() {
      this.fillField('Name',faker.name.firstName());
 
 
+   },
+   metadataPlugin: function()
+   {
+    this.fillField('Plugin Name',faker.name.title());
+
+   },
+   metadataVersion: function()
+   {
+    this.fillField('Plugin Version',faker.random.float({min:1.0, max:2.0}));
+
+   },
+   metadataPHP: function()
+   {
+    this.click('//*[@id="php"]/div/div'); 
+    this.fillField('//*[@id="php"]/div/div',faker.random.arrayElement(["7.1","7.2","7.3","7.4"]));
+
+   },
+   metadataWordpress: function()
+   {
+    this.click('//*[@id="requires"]/div/div'); 
+    this.fillField('//*[@id="requires"]/div/div',faker.random.arrayElement(["5.1","5.2","5.3","5.4","5.5","5.6"]));
+
+   },
+   metadataTested: function()
+   {
+    this.click('//*[@id="tested"]/div/div'); 
+    this.fillField('//*[@id="tested"]/div/div',faker.random.arrayElement(["5.1","5.2","5.3","5.4","5.5","5.6"]));
+
    }
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
