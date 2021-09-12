0e27268683fc512e7807be6c2de0dec0bbd427c8
alvitazwar <alvitazwar@gmail.com>
2021-09-12 19:27:22 +0600

    refactor files

--- tests/pro_plugin/dist/upload_test.dev.js
+++ tests/pro_plugin/dist/upload_test.dev.js
@@ -14,17 +14,7 @@ Scenario('upload file', function _callee(_ref) {
           I.fillField('Username', 'demo');
           I.fillField('Password', 'demo');
           I.click('Log In');
-          I.click('#rtm_show_upload_ui'); // I.usePuppeteerTo('upload action', async({ page, browser }) => {
-          //     const [fileChooser] = await Promise.all([
-          //         page.waitForFileChooser(),
-          //         page.click('#rtMedia-upload-button')
-          //     ])
-          //     await fileChooser.accept(['data/img.png']);
-          //     await page.waitForSelector('#drag-drop-area > input');
-          //     await page.click('#drag-drop-area > input');
-          //     await page.waitForTimeout(5000);
-          // }).then(console.log("file Uploaded successfully"));
-
+          I.click('#rtm_show_upload_ui');
           I.ImageUpload();
           I.amOnPage('https://qa.rtcamp.net/members/demo/media/1651/');
           I.wait(4);
--- tests/pro_plugin/upload_test.js
+++ tests/pro_plugin/upload_test.js
@@ -6,17 +6,6 @@ Scenario('upload file', async({ I }) => {
     I.fillField('Password', 'demo');
     I.click('Log In');
     I.click('#rtm_show_upload_ui');
-    // I.usePuppeteerTo('upload action', async({ page, browser }) => {
-    //     const [fileChooser] = await Promise.all([
-    //         page.waitForFileChooser(),
-    //         page.click('#rtMedia-upload-button')
-    //     ])
-    //     await fileChooser.accept(['data/img.png']);
-    //     await page.waitForSelector('#drag-drop-area > input');
-    //     await page.click('#drag-drop-area > input');
-    //     await page.waitForTimeout(5000);
-    // }).then(console.log("file Uploaded successfully"));
-
     I.ImageUpload();
     I.amOnPage('https://qa.rtcamp.net/members/demo/media/1651/');
     I.wait(4);
