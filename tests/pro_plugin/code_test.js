ab62aba2cb12971128b297a04ec656a9842b5cae
alvitazwar <alvitazwar@gmail.com>
2021-09-22 09:08:06 +0600

    add reusable payload

--- tests/pro_plugin/code_test.js
+++ tests/pro_plugin/code_test.js
@@ -2,15 +2,13 @@ Feature('All the test codes are held');
 var slugify = require('slugify');
 var faker = require('faker');
 Scenario('Experiment ZOne', async({ I }) => {
-    for (let i = 0; i <= 2; i++) {
+    for (let i = 0; i <= 1; i++) {
         // var variable = faker.datatype.number({ min: 10, max: 20 });
         // var slug_var = faker.datatype.uuid(10);
         // var seeds = faker.seed(142);
-        // var cre = faker.helpers.creditCardNumber(provider = 'amex');
-
-
+        var city = Date();
         // console.log('The value of variable', variable);
         // console.log('the value of slug', slug_var);
-        console.log('SEED', cre);
+        console.log('I am currently in', city);
     }
 })
--- tests/pro_plugin/dist/code_test.dev.js
+++ tests/pro_plugin/dist/code_test.dev.js
@@ -7,21 +7,21 @@ var slugify = require('slugify');
 var faker = require('faker');
 
 Scenario('Experiment ZOne', function _callee(_ref) {
-  var I, i;
+  var I, i, city;
   return regeneratorRuntime.async(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
           I = _ref.I;
 
-          for (i = 0; i <= 2; i++) {
+          for (i = 0; i <= 1; i++) {
             // var variable = faker.datatype.number({ min: 10, max: 20 });
             // var slug_var = faker.datatype.uuid(10);
             // var seeds = faker.seed(142);
-            // var cre = faker.helpers.creditCardNumber(provider = 'amex');
-            // console.log('The value of variable', variable);
+            city = Date(); // console.log('The value of variable', variable);
             // console.log('the value of slug', slug_var);
-            console.log('SEED', cre);
+
+            console.log('I am currently in', city);
           }
 
         case 2:
